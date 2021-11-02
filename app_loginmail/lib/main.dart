5d3ea7e64b5685399ad3552cb4d980eaaaa42ca1
Your Name <you@example.com>
2021-11-02 15:15:05 +0700

    UpdateApp_Email CongHau

---
+++ app_loginmail/lib/main.dart
@@ -0,0 +1,160 @@
+// ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers, deprecated_member_use, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(const HeroApp());
+
+class HeroApp extends StatelessWidget {
+  const HeroApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Transition Demo',
+      home: FirstRoute(),
+    );
+  }
+}
+
+class FirstRoute extends StatelessWidget {
+  const FirstRoute({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First Route'),
+      ),
+      backgroundColor: Colors.cyan,
+      body: Center(
+          child: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.only(top: 70, bottom: 20),
+            child: SizedBox(
+              height: 120,
+              width: 120,
+              child: Container(
+                  child: FlatButton(
+                onPressed: () {},
+                color: Colors.white,
+                child: Icon(
+                  Icons.drafts_sharp,
+                  size: 70,
+                  color: Colors.lightBlue.shade200,
+                ),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(60.0)),
+              )),
+            ),
+          ),
+          Container(
+            child: Text(
+              "SignIn",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 40,
+              ),
+            ),
+          ),
+          Container(
+            child: Text(
+              "Speak, friend, and enter",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 20,
+              ),
+            ),
+          ),
+          Container(
+              margin: EdgeInsets.only(top: 50),
+              width: 425,
+              child: Column(
+                children: [
+                  TextField(
+                    textAlign: TextAlign.center,
+                    decoration: const InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        suffixIcon: Icon(Icons.check_box_outline_blank),
+                        border: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white)),
+                        hintText: 'Email'),
+                  ),
+                  // Checkbox(value: true, onChanged: (value) {}),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  TextField(
+                    textAlign: TextAlign.center,
+                    decoration: const InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        border: OutlineInputBorder(),
+                        hintText: 'Password'),
+                  ),
+                ],
+              )),
+          Container(
+            width: 425,
+            height: 50,
+            margin: EdgeInsets.only(top: 100),
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Colors.black, // background
+              ),
+              child: Text('SIGN IN',
+                  style: TextStyle(
+                    color: Colors.white,
+                  )),
+              onPressed: () {
+                // Navigate to second route when tapped.
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => SecondRoute(),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      )),
+    );
+  }
+}
+
+class SecondRoute extends StatelessWidget {
+  const SecondRoute({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("TRY AGAIN"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Container(
+              margin: EdgeInsets.only(bottom: 50),
+              child: ElevatedButton(
+                onPressed: () {
+                  // Navigate back to first route when tapped.
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => FirstRoute(),
+                    ),
+                  );
+                },
+                child: Text('TRY AGAIN"'),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
