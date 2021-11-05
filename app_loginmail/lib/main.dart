fe1f9497c169c94d915a97fa699d4d6d7f1209f4
Your Name <you@example.com>
2021-11-05 15:42:42 +0700

    update Giao diện Huỳnh CÔng Hậu

--- app_loginmail/lib/main.dart
+++ app_loginmail/lib/main.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers, deprecated_member_use, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 
@@ -138,24 +139,24 @@ class SecondRoute extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
             Container(
-            margin: EdgeInsets.only(top: 70, bottom: 20),
-            child: SizedBox(
-              height: 120,
-              width: 120,
-              child: Container(
-                  child: FlatButton(
-                onPressed: () {},
-                color: Colors.white,
-                child: Icon(
-                  Icons.drafts_sharp,
-                  size: 70,
-                  color: Colors.lightBlue.shade200,
-                ),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(60.0)),
-              )),
+              margin: EdgeInsets.only(top: 70, bottom: 20),
+              child: SizedBox(
+                height: 120,
+                width: 120,
+                child: Container(
+                    child: FlatButton(
+                  onPressed: () {},
+                  color: Colors.white,
+                  child: Icon(
+                    Icons.drafts_sharp,
+                    size: 70,
+                    color: Colors.lightBlue.shade200,
+                  ),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(60.0)),
+                )),
+              ),
             ),
-          ),
             Container(
               margin: EdgeInsets.only(bottom: 2),
               child: Text(
@@ -181,7 +182,7 @@ class SecondRoute extends StatelessWidget {
               child: Text(
                 "Please, try again",
                 style: TextStyle(
-                 color: Colors.grey.shade400,
+                  color: Colors.grey.shade400,
                   fontSize: 15,
                 ),
               ),
@@ -189,7 +190,7 @@ class SecondRoute extends StatelessWidget {
             Container(
               width: 425,
               height: 50,
-              margin: EdgeInsets.only(top: 250,bottom: 10),
+              margin: EdgeInsets.only(top: 250, bottom: 94),
               child: ElevatedButton(
                 style: ElevatedButton.styleFrom(
                   primary: Colors.black, // background
@@ -215,4 +216,3 @@ class SecondRoute extends StatelessWidget {
     );
   }
 }
-
